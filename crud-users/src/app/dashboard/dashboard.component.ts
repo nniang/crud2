b66ecb359765638110374cd1a380fcca356f69f8
ngagne <ngagneniang13@gmail.com>
2023-01-24 12:40:24 +0100

    first commit

---
+++ crud-users/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent {
+  /** Based on the screen size, switch from standard to one column per row */
+  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => {
+      if (matches) {
+        return [
+          { title: '', cols: 1, rows: 1 },
+          { title: '', cols: 1, rows: 1 },
+          { title: '', cols: 1, rows: 1 },
+          { title: '', cols: 1, rows: 1 }
+        ];
+      }
+
+      return [
+        { title: '', cols: 1, rows: 1 },
+        { title: '', cols: 1, rows: 1 },
+        { title: '', cols: 1, rows: 1 },
+        { title: '', cols: 1, rows: 1 }
+      ];
+    })
+  );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+}
---
+++ crud-users/src/app/nav-sidebar/nav-sidebar.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-nav-sidebar',
+  templateUrl: './nav-sidebar.component.html',
+  styleUrls: ['./nav-sidebar.component.scss']
+})
+export class NavSidebarComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
